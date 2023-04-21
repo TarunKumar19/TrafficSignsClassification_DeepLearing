1a4fc446b9db9509c4707c44965dcab8e8f964c6
Tarun Kumar <109733908+TarunKumar19@users.noreply.github.com>
2023-04-21 15:24:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def upload():
         result = image_processing(file_path)
         s = [str(i) for i in result]
         a = int("".join(s))
-        result = "Predicted Traffic🚦Sign is: " +classes[a]
+        result = "Predicted Traffic🚦Sign is : " +classes[a]
         os.remove(file_path)
         return result
     return None
